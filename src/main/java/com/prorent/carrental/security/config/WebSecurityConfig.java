9e635b88189eb50a2a34d4eec592874cb32e3815
Samet Öztürk <0zturksamet4901@gmail.com>
2022-05-30 16:34:14 +0200

    web security config

---
+++ src/main/java/com/prorent/carrental/security/config/WebSecurityConfig.java
@@ -0,0 +1,81 @@
+package com.prorent.carrental.security.config;
+
+import java.util.Arrays;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import com.prorent.carrental.security.AuthEntryPointJwt;
+import com.prorent.carrental.security.AuthTokenFilter;
+import com.prorent.carrental.security.service.UserDetailsServiceImpl;
+
+import lombok.AllArgsConstructor;
+
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private UserDetailsServiceImpl userDetailsServiceImpl;
+	
+	private final AuthEntryPointJwt unauthorizedHandler;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean 
+	public AuthTokenFilter authenticationJwtTokenFilter() {
+		return new AuthTokenFilter();
+	} 
+	
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception{
+		return super.authenticationManager();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsServiceImpl)
+		.passwordEncoder(passwordEncoder());
+	}
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().cors().disable().sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		.authorizeHttpRequests().antMatchers("/register").permitAll()
+		.anyRequest().authenticated();
+		
+		
+		
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		
+	}
+	
+	
+}

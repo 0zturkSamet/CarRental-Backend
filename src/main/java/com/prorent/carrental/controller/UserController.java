74c9a9cb5ff8a65b6a6d212b0a39d62bc73ea967
Samet Öztürk <0zturksamet4901@gmail.com>
2022-06-05 23:12:53 +0200

    user contoller added

---
+++ src/main/java/com/prorent/carrental/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.prorent.carrental.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+//@AllArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+	
+}
